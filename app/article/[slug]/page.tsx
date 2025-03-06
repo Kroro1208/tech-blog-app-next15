f41e05d14197e21b8eae6f8a8fa8a2785e479808
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-03-06 20:35:25 +0900

    Next15の挙動確認

---
+++ app/article/[slug]/page.tsx
@@ -0,0 +1,10 @@
+type ArticleParamas = Promise<{ slug: string }>; // Next15からparamsはPromiseとして扱う
+
+export default async function ArticlePage({
+  params,
+}: {
+  params: ArticleParamas;
+}) {
+  const { slug } = await params;
+  return <h1>{slug}記事ページ</h1>;
+}
